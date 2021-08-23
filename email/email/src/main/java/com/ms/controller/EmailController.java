e66c264182c98b56a2f1566b676767510b0b3357
Luiz Calazans <luiz.calazans@viaflow.com.br>
2021-08-23 17:02:45 -0300

    Add request mapping

--- email/email/src/main/java/com/ms/controller/EmailController.java
+++ email/email/src/main/java/com/ms/controller/EmailController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/email-service")
 public class EmailController {
 
     @Autowired
